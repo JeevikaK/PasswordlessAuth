eddd8558dca4d26db44ad2e7d50f5ba96ed03576
Jeevika Kiran <jeevika.kiran@gmail.com>
2023-03-01 21:43:16 +0530

    spelling2

--- reactapp/src/components/LoginComponent.js
+++ reactapp/src/components/LoginComponent.js
@@ -65,7 +65,7 @@ const LoginComponent = () => {
                             </div>
                             <button type="submit" className="w-full text-white bg-neutral-800 shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
                             <p className="text-sm font-light text-gray-200 dark:text-gray-400">
-                                Don’t have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login up</a>
+                                Don’t have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
                             </p>
                         </form>
                     </div>
