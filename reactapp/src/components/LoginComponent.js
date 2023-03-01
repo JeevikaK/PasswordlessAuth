d2f71050308731ebfd903ccf0be68e9e7abf8bdf
Jeevika Kiran <jeevika.kiran@gmail.com>
2023-03-01 21:42:00 +0530

    spelling

--- reactapp/src/components/LoginComponent.js
+++ reactapp/src/components/LoginComponent.js
@@ -16,7 +16,7 @@ const LoginComponent = () => {
                 <div className="w-full bg-gray-800 rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                     <div className="p-6 space-y-4 md:space-y-6 sm:p-5">
                         <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-400 md:text-2xl dark:text-white">
-                            Login in to your account
+                            Login into your account
                         </h1>
                         <form className="space-y-4 md:space-y-6" action="#">
                             <div>
