123859b1472614ce859a71cd971dfb584dab3d57
Jeevika Kiran <jeevika.kiran@gmail.com>
2023-03-02 19:50:25 +0530

    front end for video

--- reactapp/src/components/VideoRecordComponent.js
+++ reactapp/src/components/VideoRecordComponent.js
@@ -1,24 +1,84 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 
-const VideoRecordComponent = () => {
-    const videoRef = useRef(null);
+function Camera() {
+  const videoRef = useRef(null);
+  const [photo, setPhoto] = useState(null);
 
-    useEffect(() => {
-      async function setupVideoStream() {
-        try {
-          const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-          if (videoRef.current) {
-            videoRef.current.srcObject = stream;
-          }
-        } catch (error) {
-          console.error('Error accessing user media:', error);
-        }
-      }
-  
-      setupVideoStream();
-    }, []);
-  
-    return <video ref={videoRef} autoPlay muted playsInline />;
+  const startCamera = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+      videoRef.current.srcObject = stream;
+    } catch (err) {
+      console.error('Error accessing camera:', err);
+    }
+  };
+
+  const stopCamera = () => {
+    const stream = videoRef.current.srcObject;
+    const tracks = stream.getTracks();
+    tracks.forEach(track => track.stop());
+    videoRef.current.srcObject = null;
+  };
+
+  const takePhoto = () => {
+    const canvas = document.createElement('canvas');
+    canvas.width = videoRef.current.videoWidth;
+    canvas.height = videoRef.current.videoHeight;
+    const ctx = canvas.getContext('2d');
+    ctx.drawImage(videoRef.current, 0, 0);
+    const dataUrl = canvas.toDataURL();
+    setPhoto(dataUrl);
+  };
+
+  return (
+    <div className="h-screen bg-black overflow-auto">
+      <h1 className='text-xl font-bold leading-tight tracking-tight text-gray-400 pr-4 pt-4 pl-4 md:text-4xl dark:text-white'>Register your Face</h1>
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0">
+        <div className='bg-gray-800 p-5 mt-8'>
+        <form className='flex flex-col items-center justify-center'>
+          <video ref={videoRef} autoPlay style={{ width: '500px', height: '400px' }} />
+          <div className='flex space-x-4'>
+            <button
+              onClick={startCamera}
+              type="button"
+              data-te-ripple-init
+              data-te-ripple-color="light"
+              className="inline-block rounded bg-neutral-800 px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-gray-300 shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]">
+              Start Camera
+            </button>
+            {/* <button 
+              onClick={stopCamera}
+              type="button"
+              data-te-ripple-init
+              data-te-ripple-color="light"
+              className="inline-block rounded bg-neutral-800 px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-gray-300 shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]">
+              Stop Camera
+            </button> */}
+            <button
+              onClick={takePhoto}
+              type="button"
+              data-te-ripple-init
+              data-te-ripple-color="light"
+              className="inline-block rounded bg-neutral-800 px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-gray-300 shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]">
+              Take Photo
+            </button>
+          </div>
+          {photo && <img src={photo} style={{ width: '400px', height: '300px' }} className='pt-5 pb-5' />}
+          {photo && <button
+              onClick={takePhoto}
+              type="button"
+              data-te-ripple-init
+              data-te-ripple-color="light"
+              className="inline-block rounded bg-neutral-800 px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-gray-300 shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]">
+              Save
+          </button>}
+        </form>
+        </div>
+
+      </div>
+    </div>
+
+  );
 }
- 
-export default VideoRecordComponent;
+
+export default Camera;
